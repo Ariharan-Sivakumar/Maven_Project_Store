b6e1b9adc16804509ba0f34945193551d2b74f12
Ariharan-Sivakumar <harisiva43@gmail.com>
2024-05-23 12:59:47 +0530

    First Commit for uploading Maven to GitHub

---
+++ mGHConn_Art/src/main/java/mavenGitHubConnGroup/mGHConn_Art/App.java
@@ -0,0 +1,13 @@
+package mavenGitHubConnGroup.mGHConn_Art;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Connecting Maven " );
+    }
+}
---
+++ mGHConn_Art/src/test/java/mavenGitHubConnGroup/mGHConn_Art/AppTest.java
@@ -0,0 +1,7 @@
+package mavenGitHubConnGroup.mGHConn_Art;
+
+public class AppTest {
+	public static void main(String[] args) {
+		System.out.println("Uploading maven project on Github");
+	}
+}
